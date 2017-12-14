ccbd6ed98eef08caa787f1aa46b2a6b2a0eb3731
LIVEU-TAD\eugenio.salguero <eugenio.salguero@live.u-tad.com>
2017-12-14 20:44:29 +0100

    no message

--- UsingFibonacci_dll/UsingFibonacci_dll.cpp
+++ UsingFibonacci_dll/UsingFibonacci_dll.cpp
@@ -15,7 +15,14 @@ int main()
 
 	cout << "Resultado Fibonacci_recursivo(10): " <<fibonacci_recursivo(10) << endl << endl;
 
+	for (int i = 0; i <= 10; i++)
+		cout << "Resultado fibonacci_DivideAndConquer(" << i <<"): " << fibonacci_DivideAndConquer(i) << endl << endl;
 
-    return 0;
+	// Maximo numero que se puede calcular sin overflow (Devuelve double)
+	cout << "Resultado fibonacci_DivideAndConquer(46): " << fibonacci_DivideAndConquer(46) << endl << endl;
+
+
+	return 0;
+
 }
 
