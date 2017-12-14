99b18c855d5244ffc91bd249edd7d2e9d046ec5a
LIVEU-TAD\eugenio.salguero <eugenio.salguero@live.u-tad.com>
2017-12-14 19:44:06 +0100

    no message

--- UsingFibonacci_dll/UsingFibonacci_dll.cpp
+++ UsingFibonacci_dll/UsingFibonacci_dll.cpp
@@ -2,10 +2,20 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+#include "Fibonacci_dll.h"
+
+using namespace std;
 
 int main()
 {
+
+	cout << "Resultado Fibonacci(10): " << fibonacci(10) << endl << endl;
+
+	cout << "Resultado Fibonacci_recursivo(10): " <<fibonacci_recursivo(10) << endl << endl;
+
+
     return 0;
 }
 
